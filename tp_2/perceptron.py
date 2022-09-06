405d249f1a4d1ece41b827624cf0f1eb5b62f875
diegoMayorga <diego.mayorga86@gmail.com>
2022-09-06 16:40:53 -0300

    Modifico perceptron

--- tp_2/perceptron.py
+++ tp_2/perceptron.py
@@ -1,42 +1,77 @@
 import argparse
 import math
+import matplotlib.pyplot as plt
+import numpy as np
 import random
 
 
 def getInitialWeights():
-  w0 = float.__round__(random.random(),2)
-  w1 = float.__round__(random.random(),2)
-  w2 = float.__round__(random.random(),2)
-  # w0 = 0.86
-  # w1 = 0.66
-  # w2 = 0.36
-  return w0,w1,w2
+  # w0 = float.__round__(random.random(),2)
+  # w1 = float.__round__(random.random(),2)
+  # w2 = float.__round__(random.random(),2)
+  weights = [
+    0.9,
+    0.66,
+    -0.2,
+  ]
+  return weights
 
-def training(desired_ouput, Lr, A, B):
-  w0,w1,w2 = getInitialWeights()
-  print(f"w0: {w0}, w1: {w1}, w2: {w2}")
+
+def getX(inputs, weights):
+  x = 0
+  for index, input in enumerate(inputs):
+    x += input * weights[index]
+  return x
+
+
+def train(desired_ouput, Lr, inputs, weights, errors, all_time_errors, all_time_real_outputs):
+  real_output_response = [1] * 4
+  for i in range(len(desired_ouput)):
+    sum_x = getX(inputs[i], weights)
+    real_output = 1 / (1 + math.exp(-sum_x))
+    real_output_response[i] = real_output
+    all_time_real_outputs.append(real_output)
+    errorValue = desired_ouput[i] - real_output
+    all_time_errors.append(errorValue)
+    errors[i] = errorValue
+    delta_calculo = real_output * (1 - real_output) * errorValue
+    for j in range(len(weights)):
+      deltaX = Lr * inputs[i][j] * delta_calculo
+      weights[j] = weights[j] + deltaX
+  return errors, weights, real_output_response, all_time_errors, all_time_real_outputs
+
+
+def createGraph(errors):  
+  x = np.arange(0, len(errors))
+  y = errors
+
+  fig, ax = plt.subplots()
+  ax.plot(x, y)
+  plt.show()
+
+
+def CheckForLess(errorList, tolerance):
+  sum_err = 0
+  for x in errorList:
+    sum_err += abs(x)
+  final_err = sum_err / 3
+  if final_err <= tolerance:
+    return True
+
+
+def main(desired_ouput, Lr, inputs):
   count = 0
-  error = True
-  errorValue = 0
-  while error:
-    error = False
-    currentEntries = len(desired_ouput)
-    for i in range(currentEntries):
-      real_output = (1 * w0) + (A[i] * w1) + (B[i] * w2)
-      y = 1 / (1 + (math.exp(-real_output)))
-      if (y != desired_ouput[i]):
-        error = True
-        errorValue = desired_ouput[i] - real_output
-        delta_calculo = real_output * (1 - real_output) * errorValue
-        deltaX = Lr * 1 * delta_calculo
-        w0 = w0 + deltaX
-        w1 = w1 + deltaX
-        w2 = w2 + deltaX
-        count += 1
-      else:
-        error = False
-      print(f"Iteraciones: {count}")
-  return count, y, real_output, errorValue
+  weights = getInitialWeights()
+
+  errors = [0] * 4
+  all_time_errors = []
+  all_time_real_outputs = []
+  while True:
+    count += 1
+    errors, weights, real_output_response, all_time_errors, all_time_real_outputs = train(desired_ouput, Lr, inputs, weights, errors, all_time_errors, all_time_real_outputs)
+    err = CheckForLess(errors, 0.1)
+    if err:
+      return errors, weights, real_output_response, all_time_errors, all_time_real_outputs, count
 
 
 if __name__ == "__main__":
@@ -52,8 +87,13 @@ if __name__ == "__main__":
   andOption = args.andGate
   xorOPtion = args.xorGate
   gateToLearn = ""
-  A = [0,0,1,1]
-  B = [0,1,0,1]
+
+  inputs = [
+    [1, 0, 0],
+    [1, 0, 1],
+    [1, 1, 0],
+    [1, 1, 1]
+  ]
   orGateOutput = [0,1,1,1]
   andGateOutput = [0,0,0,1]
   xorGateOutput = [0,1,1,0]
@@ -65,9 +105,12 @@ if __name__ == "__main__":
   elif xorOPtion:
     gateToLearn = xorGateOutput
   
-  count, y, real_output, errorValue = training(gateToLearn, learning_rate, A, B)
+  errors, weights, real_output_response, all_time_errors, all_time_real_outputs, count = main(gateToLearn, learning_rate, inputs)
+  
 
   print(f"Iteraciones: {count}")
-  print(f"Y: {y}")
-  print(f"Salida Real: {real_output}")
-  print(f"Error: {errorValue}")
+  print(f"Pesos finales: {weights}")
+  print(f"Salida Real: {real_output_response}")
+  print(f"Errores finales: {errors}")
+  createGraph(all_time_errors)
+  # createGraph(all_time_real_outputs)
